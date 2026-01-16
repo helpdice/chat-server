ea2ad3598f951046bf7fc10af88173640f884842
helpdice <169479792+helpdice@users.noreply.github.com>
2026-01-16 17:59:18 +0530

    fixed mongodb uri connect

--- connectDB/connectDb.js
+++ connectDB/connectDb.js
@@ -2,12 +2,13 @@ const mongoose = require('mongoose');
 
 const connectDB = async (dbname) => {
     try {
-        let conn = await mongoose.connect(`${process.env.MONGO_URL}/${dbname}`, {
-            useNewUrlParser: true,
-            useCreateIndex: true,
-            useUnifiedTopology: true,
-            useFindAndModify: false
-        });
+        let conn = await mongoose.connect(`${process.env.MONGO_URL}`);
+        // {
+        //     useNewUrlParser: true,
+        //     useCreateIndex: true,
+        //     useUnifiedTopology: true,
+        //     useFindAndModify: false
+        // }
         console.table({
             mongoDB: 'connected',
             host: conn.connection.host,
